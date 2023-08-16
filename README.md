9697406c3d6f68a00b30a3c7d3356a6cdd5861f6
Fransys1234 <ciocarlangalina1971@gmail.com>
2023-08-16 20:34:52 +0300

    commit3

---
+++ README.md
@@ -0,0 +1,40 @@
+
+## Grocery
+
+- Grocery-app  is a simple web application that allows users to manage their grocery lists online. Users can add, edit, and delete items from their grocery list, and the application provides a user-friendly interface for easy interaction.
+
+### Table of Contents
+- Features
+- Usage
+- Installation
+- Contributing
+#### Features
+- Add new grocery items to the list.
+- Edit existing items in the list.
+- Delete items from the list.
+- Clear the entire list.
+- Items are stored in local storage, so they are retained even after closing the browser.
+- Responsive design for seamless usage on both desktop and mobile devices.
+#### Usage
+- Visit the Grocery Bud application.
+- In the input field, enter the name of the grocery item you want to add and click the "Submit" button.
+- The item will be added to the list below the input field.
+- To edit an item, click the edit button (pencil icon) next to the item. - - Make your changes and click "Edit".
+- To delete an item, click the delete button (trash icon) next to the item.
+- To clear the entire list, click the "Clear Items" button at the bottom.
+#### Installation
+- Clone the repository:
+
+1. Copy code
+git clone [repository_url]
+Navigate to the project directory:
+
+2. Copy code
+cd grocery-bud
+Open the index.html file in a web browser.
+
+3. Fork the repository.
+- Create a new branch for your feature or bug fix: git checkout -b feature-name.
+- Make your changes and commit them: git commit -m "Add feature".
+- Push your changes to your fork: git push origin feature-name.
+- Create a pull request on the original repository.
--- app.js
+++ app.js
@@ -1,11 +1,31 @@
 // ****** select items **********
 
+//form: This variable holds a reference to the form element with the class name "grocery-form". 
+//The form element is used to allow users to input new grocery items.
 const form = document.querySelector(".grocery-form");
+
+//alert: This variable holds a reference to an element with the class name "alert". 
+//It's likely used to display alert messages to the user in response to different actions or inputs.
 const alert = document.querySelector(".alert");
+
+//grocery: This variable holds a reference to an input element with the ID "grocery". 
+//This input field is where users can enter the name of a new grocery item.
 const grocery = document.getElementById("grocery");
+
+//submitBtn: This variable holds a reference to a button element with the class name "submit-btn". 
+//This button is used to submit the form after entering a new grocery item.
 const submitBtn = document.querySelector(".submit-btn");
+
+//container: This variable holds a reference to an element with the class name "grocery-container". 
+//This likely represents the container that holds the entire grocery list interface.
 const container = document.querySelector(".grocery-container");
+
+//list: This variable holds a reference to an element with the class name "grocery-list". 
+//This is the container where individual grocery list items are displayed.
 const list = document.querySelector(".grocery-list");
+
+//clearBtn: This variable holds a reference to a button element with the class name "clear-btn". 
+//This button is used to clear the entire grocery list.
 const clearBtn = document.querySelector(".clear-btn");
 // edit option
 let editElement;
@@ -144,13 +164,17 @@ function addToLocalStorage(id, value) {
   items.push(grocery);
   localStorage.setItem("list", JSON.stringify(items));
 }
-
+//getLocalStorage(): This function retrieves the current list of items from the local storage. 
+//It checks if the "list" key exists in local storage, and if it does, 
+//it parses the stored JSON data to return an array of items. If the key doesn't exist, it returns an empty array.
 function getLocalStorage() {
   return localStorage.getItem("list")
     ? JSON.parse(localStorage.getItem("list"))
     : [];
 }
-
+//removeFromLocalStorage(id): This function removes an item from the local storage based on the provided id. 
+//It retrieves the current list of items from local storage using the getLocalStorage function, 
+//filters out the item with the specified id, and updates the local storage with the filtered array.
 function removeFromLocalStorage(id) {
   let items = getLocalStorage();
 
@@ -162,6 +186,10 @@ function removeFromLocalStorage(id) {
 
   localStorage.setItem("list", JSON.stringify(items));
 }
+//editLocalStorage(id, value): This function is used to edit the value of an item in the local storage. 
+//It takes an id and a new value as parameters. It retrieves the current list of items from local storage using 
+//the getLocalStorage function, iterates through the items, and if an item with the specified id is found, it updates its value.
+// Finally, it updates the local storage with the modified array.
 function editLocalStorage(id, value) {
   let items = getLocalStorage();
 
@@ -177,7 +205,8 @@ function editLocalStorage(id, value) {
 // SETUP LOCALSTORAGE.REMOVEITEM('LIST');
 
 // ****** setup items **********
-
+//setupItems(): This function is responsible for setting up the list of items in your grocery list interface. 
+//It retrieves the current list of items from the local storage using the getLocalStorage function
 function setupItems() {
   let items = getLocalStorage();
 
@@ -189,6 +218,16 @@ function setupItems() {
   }
 }
 
+//createListItem(id, value): This function is used to create an individual list item element for each grocery item. 
+//It creates an article element, sets a custom data-id attribute with the id value, 
+//adds the "grocery-item" class, and sets the inner HTML of the element to include the item's value 
+//and buttons for editing and deleting the item. Event listeners are attached to the edit and delete buttons 
+//to handle user interactions.
+
+//The edit-btn button triggers the editItem function when clicked.
+//The delete-btn button triggers the deleteItem function when clicked.
+//The created article element with the item's details and buttons is then appended to the list element, 
+//which is presumably the container for all the grocery list items.
 function createListItem(id, value) {
   const element = document.createElement("article");
   let attr = document.createAttribute("data-id");
